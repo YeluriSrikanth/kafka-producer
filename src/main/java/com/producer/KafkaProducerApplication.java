2a42c9214bc93709b625617a94d394c263b7c71c
YeluriSrikanth <githubstation@gmail.com>
2025-06-07 00:36:23 +0530

    added Two way communication
    added Two way communication

--- src/main/java/com/producer/KafkaProducerApplication.java
+++ src/main/java/com/producer/KafkaProducerApplication.java
@@ -2,6 +2,8 @@ package com.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class KafkaProducerApplication {
@@ -9,5 +11,12 @@ public class KafkaProducerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaProducerApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+
 
 }
